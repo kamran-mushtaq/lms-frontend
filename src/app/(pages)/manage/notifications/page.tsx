22bf16593f07e68be1c00f5dfaad2949308e6a12
Kamran Mushtaq <kamranmushtaqsays@gmail.com>
2025-04-17 13:09:46 +0500

    For 1st Client Review

--- src/app/(pages)/manage/notifications/page.tsx
+++ src/app/(pages)/manage/notifications/page.tsx
@@ -202,6 +202,18 @@
 //   );
 // }
 
+import Link from "next/link";
+
+import PlaceholderContent from "@/components/demo/placeholder-content";
+import { ContentLayout } from "@/components/admin-panel/content-layout";
+import {
+  Breadcrumb,
+  BreadcrumbItem,
+  BreadcrumbLink,
+  BreadcrumbList,
+  BreadcrumbPage,
+  BreadcrumbSeparator
+} from "@/components/ui/breadcrumb";
 
 
 export default function AccountPage() {
